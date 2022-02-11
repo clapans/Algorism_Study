630fa5b35b7fedf307ecd304e155d5e7040c91e7
clapans <clapans555@gmail.com>
2022-02-11 21:42:59 +0900

    0211

--- 박수근/all_code/10158.py
+++ 박수근/all_code/10158.py
@@ -4,9 +4,28 @@ w,h = map(int,sys.stdin.readline().split())
 x,y = map(int,sys.stdin.readline().split())
 t = int(sys.stdin.readline())
 cnt = 0
-state = 1
+dx,dy = 1,1
 
 while True:
-    
-
-print(*lst[t % cnt])
+    if cnt >= t:
+        if x == w or y == h:
+            print(x-(cnt-t),y-(cnt-t))
+        else:
+            print(x+(cnt-t),y+(cnt-t))
+        break
+    else:
+        if x == 0 or x == w:
+            dx = -dx
+        if y == 0 or y == h:
+            dy = -dy
+        if dx == 1 and dy == 1:
+            tmp = min(w-x,h-y)
+        elif dx == 1 and dy == -1:
+            tmp = min(w-x,y)
+        elif dx == -1 and dy == 1:
+            tmp = min(x,h-y)
+        else:
+            tmp = min(x,y)
+        cnt += tmp
+        x += dx*tmp
+        y += dy*tmp
