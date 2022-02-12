b67b7546b8cc9ba2fee2d72360929a26ab2ba736
clapans <clapans555@gmail.com>
2022-02-12 15:23:42 +0900

    0212

--- 박수근/all_code/10158.py
+++ 박수근/all_code/10158.py
@@ -4,14 +4,9 @@ w,h = map(int,sys.stdin.readline().split())
 x,y = map(int,sys.stdin.readline().split())
 t = int(sys.stdin.readline())
 cnt = 0
-r_cnt = 0
 dx,dy = 1,1
-pre_x,pre_y = x + min(w-x,h-y), y + min(w-x,h-y)
 
 while True:
-    if x == pre_x and y == pre_y and r_cnt == 1 and dx == 1 and dy == 1:
-        t = (t-cnt) % (cnt-1)
-        cnt = 0
     if cnt >= t:
         if dx * dy == 1:
             if x == w or y == h:
@@ -25,8 +20,6 @@ while True:
                 print(x-(cnt-t),y+(cnt-t))
         break
     else:
-        if x == pre_x and y == pre_y and dx == 1 and dy == 1:
-            r_cnt += 1
         dx = -dx if x == 0 or x == w else dx
         dy = -dy if y == 0 or y == h else dy
         if dx == 1 and dy == 1:
