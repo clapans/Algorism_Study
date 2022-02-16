de375363b691ddedf506805c9dc7226d28b302a1
clapans <clapans555@gmail.com>
2022-02-16 22:57:43 +0900

    0216

--- 박수근/all_code/1701.py
+++ 박수근/all_code/1701.py
@@ -1,6 +1,17 @@
-import sys
+s = input()
+res,cnt = 0,0
 
+while cnt < len(s):
+    j = 0
+    tmp = s[cnt:]
+    arr = [0] * len(tmp)
+    for i in range(1,len(tmp)):
+        while j > 0 and tmp[i] != tmp[j]:
+            j = arr[j-1]
+        if tmp[i] == tmp[j]:
+            j += 1
+            arr[i] = j
+    res = max(res,max(arr))
+    cnt += 1
 
-import sys
-
-s = sys.stdin.readline()
+print(res)
