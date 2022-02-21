6e4324ce574eb768c8e44510eabf42d21a7d44e1
clapans <clapans555@gmail.com>
2022-02-21 15:52:08 +0900

    0221

--- 박수근/all_code/1194.py
+++ 박수근/all_code/1194.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 m,n = map(int,sys.stdin.readline().split())
 arr = []
@@ -6,37 +7,33 @@ arr = []
 for t in range(m):
     arr.append(list(input()))
 
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+res = int(1e9)
+visit = [[[0]*(1 << 6) for i in range(n)] for j in range(m)]
 for i in range(m):
     for j in range(n):
         if arr[i][j] == '0':
-            z_pos = [i,j]
-        if arr[i][j] == '1':
-            o_pos = [i,j]
-
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-dic_ = {'A': 1, 'B': 2, 'C': 4, 'D': 8, 'E': 16, 'F': 32}
-
-def dfs(x,y,b,cnt):
-    global res
-    if [x,y] == o_pos:
-        res = min(res,cnt)
-    for t in range(4):
-        nw_x = x + dx[t]
-        nw_y = y + dy[t]
-        if 0 <= nw_x < m and 0 <= nw_y < n and arr[nw_x][nw_y] != '#' and not visit[nw_x][nw_y][b]:
-            if arr[nw_x][nw_y].islower():
-                b = b | (1 << dic_[arr[nw_x][nw_y].upper()])
-            elif arr[nw_x][nw_y].isupper():
-                if b & (1 << dic_[arr[nw_x][nw_y]]):
-                    pass
-                else:
-                    continue
-            visit[nw_x][nw_y][b] = 1
-            dfs(nw_x,nw_y,b,cnt+1)
-            visit[nw_x][nw_y][b] = 0
-
-res = int(1e9)
-visit = [[[0]*(1 << 6) for i in range(m)] for j in range(n)]
-dfs(z_pos[0],z_pos[1],0,0)
-print(res)
+            queue = deque([[i,j,0,0]])
+            visit[i][j][0] = 1
+            while queue:
+                tmp = queue.popleft()
+                for t in range(4):
+                    nw_x = tmp[0] + dx[t]
+                    nw_y = tmp[1] + dy[t]
+                    if 0 <= nw_x < m and 0 <= nw_y < n and arr[nw_x][nw_y] != '#' and not visit[nw_x][nw_y][tmp[2]]:
+                        if arr[nw_x][nw_y] == '1':
+                            print(tmp[3]+1)
+                            quit(0)
+                        elif arr[nw_x][nw_y].islower():
+                            visit[nw_x][nw_y][tmp[2]] = 1
+                            queue.append([nw_x,nw_y,tmp[2] | (1 << ord(arr[nw_x][nw_y]) - 97),tmp[3]+1])
+                        elif arr[nw_x][nw_y].isupper():
+                            if tmp[2] & (1 << ord(arr[nw_x][nw_y]) - 65):
+                                visit[nw_x][nw_y][tmp[2]] = 1
+                                queue.append([nw_x,nw_y,tmp[2],tmp[3]+1])
+                        else:
+                            visit[nw_x][nw_y][tmp[2]] = 1
+                            queue.append([nw_x,nw_y,tmp[2],tmp[3]+1])
+            print(-1)
