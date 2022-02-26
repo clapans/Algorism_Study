b6ebea140adcc75e6cb165538adb14649be0db97
clapans <clapans555@gmail.com>
2022-02-26 21:36:41 +0900

    0226

--- 박수근/all_code/6549.py
+++ 박수근/all_code/6549.py
@@ -2,10 +2,11 @@ import sys
 
 def tree(s,e,ix):
     if s == e:
-        seg[ix] = height[s]
+        seg[ix] = [height[s],s]
         return seg[ix]
     mid = (s+e)//2
-    seg[ix] = min(tree(s,mid,ix*2),tree(mid+1,e,ix*2+1))
+    tmp1,tmp2 = tree(s,mid,ix*2),tree(mid+1,e,ix*2+1)
+    seg[ix] = tmp1 if tmp1[0] < tmp2[0] else tmp2
     return seg[ix]
 
 def get(s,e,l,r,ix):
@@ -14,7 +15,12 @@ def get(s,e,l,r,ix):
     if l <= s and r >= e:
         return seg[ix]
     mid = (s+e)//2
-    return min(get(s,mid,l,r,ix*2),get(mid+1,e,l,r,ix*2+1))
+    tmp1,tmp2 = get(s,mid,l,r,ix*2),get(mid+1,e,l,r,ix*2+1)
+    return tmp1 if tmp1[0] < tmp2[0] else tmp2
+
+def sep_(s,e):
+    val,ix = get(1,height[0],s,e,1)
+    return max(sep_(s,ix-1),sep_(ix+1,e),val*(e-s+1)) 
 
 while True:
     height = list(map(int,sys.stdin.readline().split()))
@@ -23,12 +29,3 @@ while True:
     seg = [[] for _ in range((4*pow(int(height[0]**(1/2))+1,2)))]
     res = 0
     tree(1,height[0],1)
-    for i in range(1,height[0]+1):
-        if res > height[i] * (height[0]-i+1):
-            continue
-        for j in range(i,height[0]+1):
-            tmp = get(1,height[0],i,j,1)
-            if res > (height[0]-i+1) * tmp:
-                break 
-            res = max(res,tmp * (j-i+1))
-    print(res)
