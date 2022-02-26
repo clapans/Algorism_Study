b8248b262466dbc2444aed039ad78f55d4c4979b
clapans <clapans555@gmail.com>
2022-02-26 20:05:51 +0900

    0226

---
+++ 박수근/all_code/6549.py
@@ -0,0 +1,29 @@
+import sys
+
+def tree(s,e,ix):
+    if s == e:
+        seg[ix] = height[s]
+        return seg[ix]
+    mid = (s+e)//2
+    seg[ix] = min(tree(s,mid,ix*2),tree(mid+1,e,ix*2+1))
+    return seg[ix]
+
+def get(s,e,l,r,ix):
+    if r < s or l > e:
+        return int(1e9)
+    if l <= s and r >= e:
+        return seg[ix]
+    mid = (s+e)//2
+    return min(get(s,mid,l,r,ix*2),get(mid+1,e,l,r,ix*2+1))
+
+while True:
+    height = list(map(int,sys.stdin.readline().split()))
+    if not height[0]:
+        break
+    seg = [[] for _ in range((4*pow(int(height[0]**(1/2))+1,2)))]
+    res = 0
+    tree(1,height[0],1)
+    for i in range(1,height[0]+1):
+        for j in range(i,height[0]+1):
+            res = max(res,get(1,height[0],i,j,1) * (j-i+1))
+    print(res)
