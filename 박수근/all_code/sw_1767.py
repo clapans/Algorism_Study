8fb477ea5e951380ef6c7151c383241519aa1759
clapans <clapans555@gmail.com>
2022-03-01 19:38:45 +0900

    0301

--- 박수근/all_code/sw_1767.py
+++ 박수근/all_code/sw_1767.py
@@ -3,16 +3,17 @@ import copy
 dx = [-1,1,0,0]
 dy = [0,0,1,-1]
 
-def dfs(v,cur_arr,r_cnt):
+def dfs(v,cur_arr,r_cnt,l_cnt):
     global res
     if v == len(cores):
         if r_cnt > res[0]:
-            res = [r_cnt,sum([t.count(1) for t in cur_arr])]
+            res = [r_cnt,l_cnt]
         elif r_cnt == res[0]:
-            res = [r_cnt,min(res[1],sum([t.count(1) for t in cur_arr]))]
+            res = [r_cnt,min(res[1],l_cnt)]
     else:
         arr_save = copy.deepcopy(cur_arr)
         x,y = cores[v]
+        ch = 1
         for t in range(4):
             cnt = 1
             while True:
@@ -24,17 +25,19 @@ def dfs(v,cur_arr,r_cnt):
                     break
                 cnt += 1
             if nx == -1 or ny == -1 or nx == n or ny == n:
-                dfs(v+1,cur_arr,r_cnt+1)
+                ch = 0
+                dfs(v+1,cur_arr,r_cnt+1,l_cnt+cnt-1)
             cur_arr = copy.deepcopy(arr_save)
-        dfs(v+1,cur_arr,r_cnt)
-        
+        if ch:
+            dfs(v+1,cur_arr,r_cnt,l_cnt)
+
 for case in range(int(input())):
     n = int(input())
     arr = []
     for _ in range(n):
         arr.append(input().split())
     cores = []
-    res = [0,0]
+    res = [0,int(1e9)]
     for i in range(n):
         for j in range(n):
             if arr[i][j] == '1':
@@ -42,5 +45,5 @@ for case in range(int(input())):
                     res[0] += 1
                 else:
                     cores.append([i,j])
-    dfs(0,arr,res[0])
+    dfs(0,arr,res[0],0)
     print(f'#{case+1} {res[1]}')
