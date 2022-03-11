ea9c351b202d93451bb3e2b6f43b7b089fc321f0
clapans <clapans555@gmail.com>
2022-03-11 19:45:16 +0900

    0311

--- 박수근/all_code/sw_2112.py
+++ 박수근/all_code/sw_2112.py
@@ -1,7 +1,7 @@
 def improve_check(arr):
+    check_pass = [False] * w
     for i in range(w):
         tmp = 1
-        check_pass[i] = False
         for j in range(1,d):
             if arr[j][i] == arr[j-1][i]:
                 tmp += 1
@@ -10,29 +10,22 @@ def improve_check(arr):
             if tmp >= k:
                 check_pass[i] = True
                 break
-
-def change_check(pre_arr,arr):
-    for i in range(w):
-        if pre_arr[i] == False and arr[i] == True:
-            return True
-    return False
-
-def backtrack(x,cur_arr,cnt):
+    return check_pass
+ 
+def backtrack(x,cur_atr,cnt):
     global res
-    if x == d:
-        improve_check(cur_arr)
-        if check_pass == [True] * w:
-            res = min(res,cnt)
+    check_pass = improve_check(arr)
+    if check_pass == [True] * w:
+        res = min(res,cnt)
     else:
-        arr_save = [t[:] for t in cur_arr]
-        for i in range(2):
-            cur_arr[x][:] = [i] * w
-            pre_check_pass = check_pass[:]
-            improve_check(cur_arr)
-            if change_check(pre_check_pass,check_pass):
-                backtrack(x+1,cur_arr,cnt+1)
-            cur_arr = [t[:] for t in arr_save]
-        backtrack(x+1,cur_arr,cnt)
+        if x < d and cnt < res:
+            save = arr[x][:]
+            for i in range(2):
+                if cur_atr == -1 or i == cur_atr:
+                    arr[x][:] = [i] * w
+                    backtrack(x+1,i,cnt+1)
+                    arr[x][:] = save[:]
+            backtrack(x+1,-1,cnt)
 
 for case in range(int(input())):
     d,w,k = map(int,input().split())
@@ -40,6 +33,5 @@ for case in range(int(input())):
     res = int(1e9)
     for _ in range(d):
         arr.append(list(map(int,input().split())))
-    check_pass = [False] * w
-    backtrack(0,arr,0)
+    backtrack(0,-1,0)
     print(f'#{case+1} {res}')
