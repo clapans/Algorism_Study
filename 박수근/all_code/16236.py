7197938a14e616c41b34c8592cb6f365977c8779
clapans <clapans555@gmail.com>
2022-03-20 18:10:32 +0900

    0320

--- 박수근/all_code/16236.py
+++ 박수근/all_code/16236.py
@@ -1,25 +1,31 @@
 import sys
 from collections import deque
-from pprint import pprint
+import heapq
 
 def bfs(start):
     global res
+    able = []
+    limit = int(1e9)
     while queue:
         x,y,cnt = queue.popleft()
         for t in range(4):
             nx = x + dx[t]
             ny = y + dy[t]
-            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0 and arr[nx][ny] <= arr[start[0]][start[1]]:
-                if [nx,ny] in fishes and arr[nx][ny] < arr[start[0]][start[1]]:
-                    eat.append([nx,ny])
-                    fishes.remove([nx,ny])
-                    arr[nx][ny] = arr[start[0]][start[1]]
-                    arr[start[0]][start[1]] = 0
-                    start = [nx,ny]
-                    res += cnt + 1
-                    return start
+            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0 and arr[nx][ny] <= size:
+                if [nx,ny] in fishes and arr[nx][ny] < size and cnt < limit:
+                    heapq.heappush(able,[nx,ny])
+                    limit = cnt + 1
                 visit[nx][ny] = 1
                 queue.append([nx,ny,cnt + 1])
+    if able:
+        tmp = heapq.heappop(able)
+        eat.append(tmp)
+        fishes.remove(tmp)
+        arr[tmp[0]][tmp[1]] = 9
+        arr[start[0]][start[1]] = 0
+        start = tmp
+        res += limit
+        return start
     return 1
 
 n = int(sys.stdin.readline())
@@ -33,7 +39,6 @@ for i in range(n):
     for j in range(n):
         if arr[i][j] == 9:
             start = [i,j]
-            arr[i][j] = 2
         elif arr[i][j] > 0:
             fishes.append([i,j])
 
@@ -41,6 +46,7 @@ dx = [-1,0,0,1]
 dy = [0,-1,1,0]
 
 eat = []
+size = 2
 res = 0
 
 while True:
@@ -50,8 +56,8 @@ while True:
     start = bfs(start)
     if start == 1:
         break
-    if len(eat) == arr[start[0]][start[1]]:
-        arr[start[0]][start[1]] += 1
+    if len(eat) == size:
+        size += 1
         eat = [] 
 
 print(res)
