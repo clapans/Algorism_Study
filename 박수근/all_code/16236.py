9bce2f71c03125ec9785a4a312df8053a1cd5f5a
clapans <clapans555@gmail.com>
2022-03-20 17:51:57 +0900

    0320

--- 박수근/all_code/16236.py
+++ 박수근/all_code/16236.py
@@ -2,6 +2,26 @@ import sys
 from collections import deque
 from pprint import pprint
 
+def bfs(start):
+    global res
+    while queue:
+        x,y,cnt = queue.popleft()
+        for t in range(4):
+            nx = x + dx[t]
+            ny = y + dy[t]
+            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0 and arr[nx][ny] <= arr[start[0]][start[1]]:
+                if [nx,ny] in fishes and arr[nx][ny] < arr[start[0]][start[1]]:
+                    eat.append([nx,ny])
+                    fishes.remove([nx,ny])
+                    arr[nx][ny] = arr[start[0]][start[1]]
+                    arr[start[0]][start[1]] = 0
+                    start = [nx,ny]
+                    res += cnt + 1
+                    return start
+                visit[nx][ny] = 1
+                queue.append([nx,ny,cnt + 1])
+    return 1
+
 n = int(sys.stdin.readline())
 arr = []
 for _ in range(n):
@@ -27,24 +47,8 @@ while True:
     queue = deque([[*start,0]])
     visit = [[0]*n for _ in range(n)]
     visit[start[0]][start[1]] = 1
-    ch = 1
-    while queue:
-        x,y,cnt = queue.popleft()
-        for t in range(4):
-            nx = x + dx[t]
-            ny = y + dy[t]
-            if 0 <= nx < n and 0 <= ny < n and visit[nx][ny] == 0 and arr[nx][ny] <= arr[start[0]][start[1]]:
-                if [nx,ny] in fishes and arr[nx][ny] < arr[start[0]][start[1]]:
-                    eat.append([nx,ny])
-                    arr[nx][ny] = arr[start[0]][start[1]]
-                    arr[start[0]][start[1]] = 0
-                    start = [nx,ny]
-                    res += cnt + 1
-                    ch = 0
-                    break
-                visit[nx][ny] = 1
-                queue.append([nx,ny,cnt + 1])
-    if ch:
+    start = bfs(start)
+    if start == 1:
         break
     if len(eat) == arr[start[0]][start[1]]:
         arr[start[0]][start[1]] += 1
