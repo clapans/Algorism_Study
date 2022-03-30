216bc04e3e94b9b1208b4a9e86595dafb29a9e4a
clapans <clapans555@gmail.com>
2022-03-30 18:47:10 +0900

    0330

--- 박수근/all_code/sw_5650.py
+++ 박수근/all_code/sw_5650.py
@@ -6,29 +6,28 @@ block_dic = {1 : (1,3,0,2), 2 : (3,0,1,2), 3 : (2,0,3,1),
 
 wall = {0: 1, 1: 0, 2: 3, 3: 2}
 
-def pin_ball(x,y,dir,res):
+def pin_ball(x,y,dir):
+    res = 0
+    start = [x,y]
     while True:
         x += dx[dir]
         y += dy[dir]
         if 0 <= x < n and 0 <= y < n:
-            if len(visit) == 1 or visit[-2] != [x,y]:
-                visit.append([x,y])
-                if arr[x][y] == -1:
-                    break
-                elif 1 <= arr[x][y] <= 5:
-                    dir = block_dic[arr[x][y]][dir]
-                    res += 1
-                elif 6 <= arr[x][y]:
-                    for t in worm_hall[arr[x][y]]:
-                        if t != [x,y]:
-                            tmp = t
-                    x,y = tmp
-            else:
-                res = 2*(res-1)+1
+            if arr[x][y] == -1:
+                break
+            elif 1 <= arr[x][y] <= 5:
+                dir = block_dic[arr[x][y]][dir]
+                res += 1
+            elif 6 <= arr[x][y]:
+                for t in worm_hall[arr[x][y]]:
+                    if t != [x,y]:
+                        tmp = t
+                x,y = tmp
+            elif [x,y] == start:
                 break
         else:
-            res = 2*res+1
-            break
+            dir = wall[dir]
+            res += 1
     return res
 
 for case in range(int(input())):
@@ -48,8 +47,6 @@ for case in range(int(input())):
         for j in range(n):
             if arr[i][j] == 0:
                 for t in range(4):
-                    visit = [[i,j]]
-                    tmp = pin_ball(i,j,t,0)
-                    res = max(res,tmp)
+                    res = max(res,pin_ball(i,j,t))
 
     print(f'#{case+1} {res}')
