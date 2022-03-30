605b05430199834cd3276e39dbb756c10b105935
clapans <clapans555@gmail.com>
2022-03-30 16:49:30 +0900

    0330

--- 박수근/all_code/sw_5650.py
+++ 박수근/all_code/sw_5650.py
@@ -6,32 +6,31 @@ block_dic = {1 : (1,3,0,2), 2 : (3,0,1,2), 3 : (2,0,3,1),
 
 wall = {0: 1, 1: 0, 2: 3, 3: 2}
 
-def pin_ball(x,y,dir,res,start):
+def pin_ball(x,y,dir,res):
     while True:
-        if [x,y] == start and res != 0:
-            break
-        nx = x + dx[dir]
-        ny = y + dy[dir]
-        if 0 <= nx < n and 0 <= ny < n:
-            if arr[nx][ny] == -1:
-                break
-            elif 1 <= arr[nx][ny] <= 5:
-                res += 1
-                dir = block_dic[arr[nx][ny]][dir]
-                x,y = nx,ny
-            elif 6 <= arr[nx][ny]:
-                for t in worm_hall[arr[nx][ny]]:
-                    if t != [nx,ny]:
-                        tmp = t
-                x,y = tmp
+        x += dx[dir]
+        y += dy[dir]
+        if 0 <= x < n and 0 <= y < n:
+            if len(visit) == 1 or visit[-2] != [x,y]:
+                visit.append([x,y])
+                if arr[x][y] == -1:
+                    break
+                elif 1 <= arr[x][y] <= 5:
+                    dir = block_dic[arr[x][y]][dir]
+                    res += 1
+                elif 6 <= arr[x][y]:
+                    for t in worm_hall[arr[x][y]]:
+                        if t != [x,y]:
+                            tmp = t
+                    x,y = tmp
             else:
-                x,y = nx,ny
+                res = 2*(res-1)+1
+                break
         else:
-            dir = wall[dir]
-            res += 1
-            x,y = nx,ny
+            res = 2*res+1
+            break
     return res
-    
+
 for case in range(int(input())):
     n = int(input())
     arr = []
@@ -43,15 +42,14 @@ for case in range(int(input())):
         for j in range(n):
             if arr[i][j] >= 6:
                 worm_hall[arr[i][j]].append([i,j])
-    
+
     res = 0
     for i in range(n):
         for j in range(n):
             if arr[i][j] == 0:
-                start = [i,j]
                 for t in range(4):
-                    tmp = pin_ball(i,j,t,0,start)
-                    if tmp:
-                        res = max(res,tmp)
+                    visit = [[i,j]]
+                    tmp = pin_ball(i,j,t,0)
+                    res = max(res,tmp)
 
     print(f'#{case+1} {res}')
