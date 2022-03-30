9c4448ac4f51fa493a8e07753f6f78829b7412f9
clapans <clapans555@gmail.com>
2022-03-30 13:57:49 +0900

    0330

--- 박수근/all_code/sw_5650.py
+++ 박수근/all_code/sw_5650.py
@@ -6,26 +6,31 @@ block_dic = {1 : (1,3,0,2), 2 : (3,0,1,2), 3 : (2,0,3,1),
 
 wall = {0: 1, 1: 0, 2: 3, 3: 2}
 
-def pin_ball(x,y,dir,res):
-    nx = x + dx[dir]
-    ny = y + dy[dir]
-    if [nx,ny] == start:
-        return
-    if 0 <= nx < n and 0 <= ny < n:
-        if arr[nx][ny] == -1:
-            return res
-        elif 1 <= arr[nx][ny] <= 5:
+def pin_ball(x,y,dir,res,start):
+    while True:
+        if [x,y] == start and res != 0:
+            break
+        nx = x + dx[dir]
+        ny = y + dy[dir]
+        if 0 <= nx < n and 0 <= ny < n:
+            if arr[nx][ny] == -1:
+                break
+            elif 1 <= arr[nx][ny] <= 5:
+                res += 1
+                dir = block_dic[arr[nx][ny]][dir]
+                x,y = nx,ny
+            elif 6 <= arr[nx][ny]:
+                for t in worm_hall[arr[nx][ny]]:
+                    if t != [nx,ny]:
+                        tmp = t
+                x,y = tmp
+            else:
+                x,y = nx,ny
+        else:
+            dir = wall[dir]
             res += 1
-            dir = block_dic[arr[nx][ny]][dir]
-        elif 6 <= arr[nx][ny]:
-            for t in worm_hall[arr[nx][ny]]:
-                if t != [nx,ny]:
-                    tmp = t
-            nx,ny = tmp
-        pin_ball(nx,ny,dir,res)
-    else:
-        dir = wall[dir]
-        pin_ball(x,y,dir,res+1)
+            x,y = nx,ny
+    return res
     
 for case in range(int(input())):
     n = int(input())
@@ -45,9 +50,8 @@ for case in range(int(input())):
             if arr[i][j] == 0:
                 start = [i,j]
                 for t in range(4):
-                    tmp = pin_ball(i,j,t,0)
+                    tmp = pin_ball(i,j,t,0,start)
                     if tmp:
                         res = max(res,tmp)
 
     print(f'#{case+1} {res}')
-                    
