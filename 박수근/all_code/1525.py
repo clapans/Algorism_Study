82095222c9ca56cf57b79a8be15ad1a4906a2bdb
clapans <clapans555@gmail.com>
2022-04-26 17:34:29 +0900

    0426

--- 박수근/all_code/1525.py
+++ 박수근/all_code/1525.py
@@ -1,48 +1,41 @@
 import sys
 from collections import deque
 
-arr = []
+arr = ""
 for _ in range(3):
-    arr.append(list(map(int,sys.stdin.readline().split())))
+    arr += sys.stdin.readline().strip().replace(' ','')
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def find(arr):
-    for i in range(3):
-        for j in range(3):
-            if arr[i][j] == 0:
-                return [i,j]
-
-def trans_tuple(arr):
-    tmp = []
-    for i in range(3):
-        tmp.append((arr[i][0],arr[i][1],arr[i][2]))
-    return tuple(tmp)
-
-answer = [[1,2,3],[4,5,6],[7,8,0]]
+answer = "123456780"
 visit = {}
-queue = deque([[arr,0]])
+visit[arr] = 0
+queue = deque([arr])
 while queue:
-    tmp_ = queue.popleft()
-    arr,cnt = tmp_[0],tmp_[1]
-    if arr == answer:
-        print(cnt)
+    puzzle = queue.popleft()
+    if puzzle == answer:
+        print(visit[puzzle])
         quit(0)
-    start = find(arr)
-    arr_save = [t[:] for t in arr]
+    idx = puzzle.index("0")
+    start = [idx//3,idx%3]
     for t in range(4):
         nx = start[0] + dx[t]
         ny = start[1] + dy[t]
         if 0 <= nx < 3 and 0 <= ny < 3:
-            tmp = arr[nx][ny]
-            arr[nx][ny] = 0
-            arr[start[0]][start[1]] = tmp
-            trans = trans_tuple(arr)
+            n_puzzle = ""
+            tmp = puzzle[3*nx + ny]
+            for i in range(9):
+                if i != 3*nx+ny and i != idx:
+                    n_puzzle += puzzle[i]
+                elif i == 3*nx+ny:
+                    n_puzzle += "0"
+                else:
+                    n_puzzle += tmp
             try:
-                visit[trans]
+                visit[n_puzzle]
             except:
-                visit[trans] = 0
-                queue.append([arr,cnt+1])
-            arr = [t[:] for t in arr_save]
+                visit[n_puzzle] = visit[puzzle] + 1
+                queue.append(n_puzzle)
+            
 print(-1)
