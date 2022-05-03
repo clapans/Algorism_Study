f53f3ad67728dbe3e388721ff7d68e5537be3da9
clapans <clapans555@gmail.com>
2022-05-03 17:02:57 +0900

    0503

--- 박수근/all_code/9935.py
+++ 박수근/all_code/9935.py
@@ -1,19 +1,16 @@
 import sys
 
-s = sys.stdin.readline().rstrip()
-word = list(sys.stdin.readline().rstrip())
+word = sys.stdin.readline().rstrip()
+explode = list(sys.stdin.readline().rstrip())
 stack = []
-len_w = len(word)
 
-for i in s:
+for i in word:
     stack.append(i)
-    if len(stack) >= len_w:
-        if stack[-len(word):] == word:
-            for i in range(len_w):
-                stack.pop()
+    if len(stack) >= len(explode) and stack[-len(explode):] == explode:
+        for j in range(len(explode)):
+            stack.pop()
 
 if stack:
     print(''.join(stack))
 else:
     print('FRULA')
-
