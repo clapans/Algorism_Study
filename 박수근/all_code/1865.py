a84edade74b998dcfe911cdb0568fb3252800bce
clapans <clapans555@gmail.com>
2022-07-11 23:43:15 +0900

    0711

--- 박수근/all_code/1865.py
+++ 박수근/all_code/1865.py
@@ -1,10 +1,10 @@
 import sys
 
 def distance():
-    for mid in range(1,n+1):
+    for middle in range(1,n+1):
         for i in range(1,n+1):
             for j in range(1,n+1):
-                dp[i][j] = min(dp[i][j],dp[i][mid] + dp[mid][j])
+                dp[i][j] = min(dp[i][j],dp[i][middle] + dp[middle][j])
                 if i == j and dp[i][j] < 0:
                     return "YES"
     return "NO"
